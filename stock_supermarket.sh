d387400b3308dc6aa82ba708e72bb8fd9a9ebbae
River Pease <riverpease15@gmail.com>
2018-07-09 18:39:02 +0000

    automatically backed up by learn

--- stock_supermarket.sh
+++ stock_supermarket.sh
@@ -1,5 +1,4 @@
 ls;
-mv
-delivery/apples.txt fruit/apples.txt
-mv
-delivery/bison_burgers.txt meat/bison_burgers.txt
+mv delivery/apples.txt fruit/apples.txt;
+mv delivery/bison_burgers.txt meat/bison_burgers.txt;
+mv delivery/brie.txt 
