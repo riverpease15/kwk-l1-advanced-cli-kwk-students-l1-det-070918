cc71f77417e0d048acfc874664519e1fa35140b9
River Pease <riverpease15@gmail.com>
2018-07-09 18:41:07 +0000

    automatically backed up by learn

--- stock_supermarket.sh
+++ stock_supermarket.sh
@@ -2,3 +2,7 @@ ls;
 mv delivery/apples.txt fruit/apples.txt;
 mv delivery/bison_burgers.txt meat/bison_burgers.txt;
 mv delivery/brie.txt dairy/brie.txt;
+mv delivery/broccoli.txt veggies/broccoli.txt;
+mv delivery/carrot_cake.txt dessert/carrot_cake.txt;
+mv delivery/carrots.txt veggies/carrots.txt;
+mv delivery/eggo_waffles.txt freezer/eggo_waffles.txt;
