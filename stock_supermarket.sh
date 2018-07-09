c7267e6dd481785b7fe0d634aed7367c652f1372
River Pease <riverpease15@gmail.com>
2018-07-09 18:45:17 +0000

    automatically backed up by learn

--- stock_supermarket.sh
+++ stock_supermarket.sh
@@ -6,3 +6,12 @@ mv delivery/broccoli.txt veggies/broccoli.txt;
 mv delivery/carrot_cake.txt dessert/carrot_cake.txt;
 mv delivery/carrots.txt veggies/carrots.txt;
 mv delivery/eggo_waffles.txt freezer/eggo_waffles.txt;
+mv delivery/fage_yogurt.txt dairy/fage_yogurt.txt;
+mv delivery/fig_bars.txt snax/fig_bars.txt;
+mv delivery/flourless_cake.txt dessert/flourless_cake.txt;
+mv delivery/garlic.txt veggies/garlic.txt;
+mv delivery/goldfish.txt snax/goldfish.txt;
+mv delivery/grapples.txt fruit/grapples.txt;
+mv delivery/hotdogs.txt meat/hotdogs.txt;
+mv delivery/
+mv delivery/
